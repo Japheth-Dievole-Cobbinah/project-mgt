345abbe18ffacf5fe949998a131f7453c5afe70e
japheth cobbinah <dievole7@gmail.com>
2025-10-30 18:29:30 +0000

    routes and middleware created

--- .history/server/server_20251030182108.js
+++ .history/server/server_20251030182108.js
@@ -5,7 +5,6 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import workspaceRouter from './routes/workspaceRoutes.js';
-import { protect } from './middlewares/authMiddleware.js';
 
 const app = express();
 
@@ -18,7 +17,7 @@ app.get('/', (req, res) => res.send('Server is live'));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Routes
-app.use('/api/workspaces', protect , workspaceRouter)
+app.use('/api/workspaces', workspaceRouter)
 
 const PORT =  process.env.PORT || 5000;
 
